984dbadf6eb84cdd0b06bc9dab298facc8236282
carlos.ueda <carlos.ueda@gmail.com>
2021-06-25 22:14:44 -0400

    Project initialized

---
+++ src/main/java/com/caueda/projectbono/ProjectbonoApplication.java
@@ -0,0 +1,13 @@
+package com.caueda.projectbono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectbonoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectbonoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caueda/projectbono/ProjectbonoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caueda.projectbono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectbonoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
